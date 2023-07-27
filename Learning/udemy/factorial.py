e9f84865cfc91202c2bfb97c509c574bf310f8d6
rbhallalocal <bhalladon@gmail.com>
2023-07-28 00:04:38 +0530

    formatting

--- Learning/udemy/factorial.py
+++ Learning/udemy/factorial.py
@@ -4,9 +4,9 @@ class Factorial:
         factorial_number = int(input("Please enter a integer value to find the factorial: "))
         global var
         var = 1
-        for i in range(1, factorial_number+1):
+        for i in range(1, factorial_number + 1):
             var = var * i
-        print ("Factorial of the number " + str(factorial_number) + " is: " + str(var))
+        print("Factorial of the number " + str(factorial_number) + " is: " + str(var))
 
 
 a = Factorial()
